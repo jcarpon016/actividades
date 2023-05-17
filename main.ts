19cf1a22fec0767edaa20e4b96d6091102a4df1d
jcarpon016 <132546629+jcarpon016@users.noreply.github.com>
2023-05-17 19:58:10 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,8 @@ basic.showString("hola")
 for (let index = 0; index < 5; index++) {
     basic.showIcon(IconNames.Heart)
     basic.pause(100)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(100)
 }
 basic.clearScreen()
 basic.showString("adios")
